a818bcf91dc26288a21c8494c2a5945a7317d8f5
Jacklu <jacklu.hdz@gmail.com>
2019-03-26 23:37:13 -0600

    Recetas terminadas

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,11 +6,17 @@ const Recipes = (props) => (
        <div className="row">
        { props.recipes.map((recipe) => {
           return (
-            <div className="col-md-4">
+            <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem"}}>
                 <div className="recipes__box">
                     <img className="recipes__box-img" src={recipe.image_url} alt={recipe.title}></img>
                     <div className="recipe__text">
-                        <h5>{ recipe.title }</h5>
+                        <h5 className="recipes__title">
+                            { recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                        </h5>
+                        <p className="recipes__subtitle">Publisher: <span>
+                            { recipe.publisher }
+                            </span></p>
+                        <button className="recipe_buttons">View Recipe</button>
                     </div>
                 </div>
             </div>
